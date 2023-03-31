ce11d6825ce19d853d5a0bd817f6d6a236259e7a
MachareteL <116664977+MachareteL@users.noreply.github.com>
2023-03-31 16:41:33 -0300

    first checkout with PIX done

---
+++ src/pages/api/checkout/createCobranca.js
@@ -0,0 +1,107 @@
+import { Agent } from "https";
+import axios from "axios";
+import { readFileSync } from "fs";
+import path from 'path'
+
+export default async function handler(req, res) {
+    if (true) {
+        const teste = req.headers
+        console.log(teste);
+        // const valor_cobranca = req.body.valor_cobranca
+    }
+    // async function getToken() {
+    //     const certificado = readFileSync(path.join(process.cwd()) + '/SenaiProd.p12');
+    //     const credenciais = {
+    //         client_id: process.env.GN_CLIENT_ID,
+    //         client_secret: process.env.GN_CLIENT_SECRET,
+    //     };
+
+    //     const data = JSON.stringify({ grant_type: "client_credentials" });
+    //     const data_credentials = credenciais.client_id + ":" + credenciais.client_secret;
+
+    //     const auth = Buffer.from(data_credentials).toString("base64");
+
+    //     const agent = new Agent({
+    //         pfx: certificado,
+    //         passphrase: "",
+    //     });
+
+    //     const config = {
+    //         method: "POST",
+    //         url: "https://api-pix.gerencianet.com.br/oauth/token",
+    //         headers: {
+    //             Authorization: "Basic " + auth,
+    //             "Content-Type": "application/json",
+    //         },
+    //         httpsAgent: agent,
+    //         data,
+    //     };
+
+    //     const result = await axios(config)
+    //     return result.data
+
+    // }
+
+    // async function createCobranca(access_token) {
+    //     const certificado = readFileSync(path.join(process.cwd()) + '/SenaiProd.p12');
+
+    //     const data = JSON.stringify({
+    //         calendario: {
+    //             expiracao: 3600,
+    //         },
+    //         devedor: {
+    //             nome: "",
+    //             cpf: '',
+    //         },
+    //         valor: {
+    //             original: valor_cobranca
+    //         },
+    //         chave: '6c857e0b-fca3-436a-9486-8c3db3fabe64',
+    //         solicitacaoPagador: 'Pagar até uma hora'
+    //     });
+
+
+    //     const agent = new Agent({
+    //         pfx: certificado,
+    //         passphrase: "",
+    //     });
+
+    //     const config = {
+    //         method: "POST",
+    //         url: "https://api-pix.gerencianet.com.br/v2/cob",
+    //         headers: {
+    //             Authorization: "Bearer " + access_token,
+    //             "Content-Type": "application/json",
+    //         },
+    //         httpsAgent: agent,
+    //         data,
+    //     };
+
+    //     const result = await axios(config)
+    //     return result
+    // }
+
+
+    // async function getLoc(access_token, locID) {
+    //     const certificado = readFileSync(path.join(process.cwd()) + '/SenaiProd.p12');
+    //     const agent = new Agent({
+    //         pfx: certificado,
+    //         passphrase: "",
+    //     });
+    //     const config = {
+    //         method: "GET",
+    //         url: "https://api-pix.gerencianet.com.br/v2/loc/"+locID+'/qrcode',
+    //         headers: {
+    //             Authorization: "Bearer " + access_token,
+    //             "Content-Type": "application/json",
+    //         },
+    //         httpsAgent: agent,
+    //     };
+    //     const result = await axios(config)
+    //     return result
+    // }
+    // const { access_token } = await getToken()
+    // const cobranca = await createCobranca(access_token)
+    // const qrcode = await getLoc(access_token, cobranca.data.loc.id)
+    // res.send(qrcode.data)
+}
